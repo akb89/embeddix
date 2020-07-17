2374d4861ed6f96ddacedeb38bd04cb1c223cca6
AKB <akb@3azouz.net>
2020-07-17 14:37:47 +0200

    Removed xp files

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     author_email='akb@3azouz.net',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='1.13.1',
+    version='1.13.2',
     url='https://github.com/akb89/embeddix',
     download_url='https://github.com/akb89/embeddix',
     license='MIT',
